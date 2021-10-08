0c141749922dc6cd99d6a72447b617c8da9b5e99
Alex Paredes <alexparedesr95@gmail.com>
2021-10-08 00:35:23 -0500

    FEAT: Se crea router para el módulo auth.

---
+++ src/modules/auth/router/index.js
@@ -0,0 +1,9 @@
+export default {
+
+    name: 'auth',
+    component: () => import(/* webpackChunkName: "Auth Layout" */ '@/modules/auth/layouts/AuthLayout.vue'),
+    children: [
+        
+    ]
+
+}
