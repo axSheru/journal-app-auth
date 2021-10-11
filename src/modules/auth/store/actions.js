9781a6622f9b4401f45bc8bea87b675012663877
Alex Paredes <alexparedesr95@gmail.com>
2021-10-11 00:52:10 -0500

    FEAT: Se crea acción para registrar un usuario en firebase.

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -1,3 +1,26 @@
+import authApi from "@/api/authApi"
+
 /* export const myAction = async ({ commit }) => {
     
 } */
+
+
+export const createUser = async ( { commit }, user ) => {
+    const { name, email, password } = user
+    console.warn(name, commit)
+
+    try {
+
+        const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
+        console.log(data)
+
+        //TODO: Mutation: loginUser
+
+        return { ok: true }
+
+    } catch (error) {
+
+        return { ok: false, message: error.response.data.error.message }
+
+    }
+}
