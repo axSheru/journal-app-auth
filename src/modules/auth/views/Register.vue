1d805218a00801e97e04b08b19939846e36f034d
Alex Paredes <alexparedesr95@gmail.com>
2021-10-11 01:36:07 -0500

    FEAT: Se controla redirección si el registro sale bien o mostrar mensaje de error si sale mal.

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -34,12 +34,16 @@
 
 <script>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import useAuth from '../composables/useAuth'
+
+import Swal from 'sweetalert2';
 
 export default {
 
     setup() {
 
+        const router = useRouter()
         const { createUser } = useAuth()
 
         const userForm = ref({
@@ -53,7 +57,9 @@ export default {
 
             onSubmit: async() => {
                 const { ok, message } = await createUser( userForm.value )
-                console.log(ok, message)
+                
+                if ( !ok ) Swal.fire( 'Error', message, 'error' )
+                else router.push({ name: 'no-entry' })
             }
         }
 
