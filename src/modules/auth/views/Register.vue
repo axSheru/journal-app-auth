11dda979823df15a2c8d4f4f6f9ffad67bd24e5e
Alex Paredes <alexparedesr95@gmail.com>
2021-10-11 00:53:20 -0500

    FEAT: Se muestra en la consola si todo salió bien o no al registrar al usuario.

--- src/modules/auth/views/Register.vue
+++ src/modules/auth/views/Register.vue
@@ -52,7 +52,8 @@ export default {
             userForm,
 
             onSubmit: async() => {
-                createUser( userForm.value )
+                const { ok, message } = await createUser( userForm.value )
+                console.log(ok, message)
             }
         }
 
