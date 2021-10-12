807948a4ff8f11c1feebf5b4cb5cc79c999f252c
Alex Paredes <alexparedesr95@gmail.com>
2021-10-11 23:59:37 -0500

    FEAT: Se crea acción signInUser.

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -26,3 +26,22 @@ export const createUser = async ( { commit }, user ) => {
 
     }
 }
+
+export const signInUser = async ( { commit }, user ) => {
+    const { email, password } = user
+
+    try {
+
+        const { data } = await authApi.post(':signInWithPassword', { email, password, returnSecureToken: true })
+        const { idToken, refreshToken } = data
+        
+        commit('loginUser', { user, idToken, refreshToken })
+
+        return { ok: true }
+
+    } catch (error) {
+
+        return { ok: false, message: error.response.data.error.message }
+
+    }
+}
