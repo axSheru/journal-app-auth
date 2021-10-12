a0746a93aa2e781802c3a7e9be328167dd86946c
Alex Paredes <alexparedesr95@gmail.com>
2021-10-12 00:00:03 -0500

    FEAT: Se utiliza action signInUser en el composable.

--- src/modules/auth/composables/useAuth.js
+++ src/modules/auth/composables/useAuth.js
@@ -10,8 +10,16 @@ const useAuth = () => {
         return resp
     }
 
+    const loginUser = async( user ) => {
+
+        const resp = await store.dispatch('auth/signInUser', user)
+        return resp
+
+    }
+
     return {
-        createUser
+        createUser,
+        loginUser,
     }
 
 }
