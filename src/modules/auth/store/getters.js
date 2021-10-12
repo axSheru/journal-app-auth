bdc998f6b122d04bf67046f6cca729ec447ffd7b
Alex Paredes <alexparedesr95@gmail.com>
2021-10-12 01:21:37 -0500

    FEAT: Se crea getter para retornar el nombre del usuario en caso de que exista.

--- src/modules/auth/store/getters.js
+++ src/modules/auth/store/getters.js
@@ -1,7 +1,11 @@
 /* export const myGetter = ( state ) => {
-    return state;
+    return state
 } */
 
 export const currentState = ( state ) => {
-    return state.status;
+    return state.status
+}
+
+export const username = ( state ) => {
+    return state.user?.name || ''
 }
