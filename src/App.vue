6132952b01259d8b18682618c3d9ddaf8e1ac3b0
Alex Paredes <alexparedesr95@gmail.com>
2021-10-12 00:55:24 -0500

    FEAT: Se consulta el state.status en la raíz de la aplicación.

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,20 @@
   <router-view/>
 </template>
 
+
+<script>
+import useAuth from './modules/auth/composables/useAuth'
+export default {
+  setup() {
+    
+    const { authStatus, checkAuthStatus } = useAuth()
+
+    checkAuthStatus()
+
+    return {
+      authStatus
+    }
+
+  }
+}
+</script>
