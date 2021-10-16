5ebf8b3c9b3237a0ac079ab7656b6e445a69df3f
Alex Paredes <alexparedesr95@gmail.com>
2021-10-15 22:29:45 -0500

    TEST: Se prueba mutation loginUser.

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -20,5 +20,33 @@ describe('Vuex: Pruebas en el auth-module.', () => {
         
     })
     
+    //Mutations.
+
+    test('Mutation: loginUSer.', () => {
+
+        const store = createVuexStore({
+            status: 'authenticating',// 'authenticated', 'not-authenticated', 'authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const payload = {
+            user: { name: 'Alex', email: 'alex@gmail.com' },
+            idToken: 'ABC-123',
+            refreshToken: 'XYZ-123'
+        }
+
+        store.commit( 'auth/loginUser', payload )
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect( status ).toBe( 'authenticated' )
+        expect( user ).toEqual( { name: 'Alex', email: 'alex@gmail.com' } )
+        expect( idToken ).toBe( 'ABC-123' )
+        expect( refreshToken ).toBe( 'XYZ-123' )
+        
+    })
+    
 
 })
