a7bddcbc50174b7c6352c9a78215f6e2a528222a
Alex Paredes <alexparedesr95@gmail.com>
2021-10-18 00:14:13 -0500

    TEST: composable useAuth - Computed: authStatus.

--- tests/unit/modules/auth/composables/useAuth.spec.js
+++ tests/unit/modules/auth/composables/useAuth.spec.js
@@ -2,7 +2,10 @@ import useAuth from '@/modules/auth/composables/useAuth'
 
 const mockStore = {
     dispatch: jest.fn(),
-    commit: jest.fn()
+    commit: jest.fn(),
+    getters: {
+        'auth/currentState': 'authenticated'
+    }
 }
 
 jest.mock('vuex', () => ({
@@ -89,6 +92,14 @@ describe('Pruebas en useAuth.', () => {
 
         expect( mockStore.commit ).toHaveBeenCalledWith( 'auth/logout' )
         expect( mockStore.commit ).toHaveBeenCalledWith( 'journal/clearEntries' )
+
+    })
+    
+    test('Computed: authStatus.', () => {
+        
+        const { authStatus } = useAuth()
+
+        expect( authStatus.value ).toBe( 'authenticated' )
 
     })
     
