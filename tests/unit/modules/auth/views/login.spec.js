2ba5e93e85ec3d35336960336e93303e3934272e
Alex Paredes <alexparedesr95@gmail.com>
2021-10-18 21:47:42 -0500

    TEST: Pruebas en login component - Credenciales incorrectas, error de autenticación.

--- tests/unit/modules/auth/views/login.spec.js
+++ tests/unit/modules/auth/views/login.spec.js
@@ -1,7 +1,16 @@
 import { shallowMount } from '@vue/test-utils'
+import Login from '@/modules/auth/views/Login'
+
 import createVuexStore from '../../../mock-data/mock-store'
 
-import Login from '@/modules/auth/views/Login'
+import Swal from 'sweetalert2'
+
+jest.mock('sweetalert2', () => ({
+    fire: jest.fn(),
+    showLoadig: jest.fn(),
+    close: jest.fn()
+}))
+
 
 describe('Pruebas en el login component.', () => {
 
@@ -11,6 +20,8 @@ describe('Pruebas en el login component.', () => {
         idToken: null,
         refreshToken: null
     })
+
+    store.dispatch = jest.fn()
     
     test('Debe de hacer match con el snapshot.', () => {
         
@@ -24,5 +35,21 @@ describe('Pruebas en el login component.', () => {
 
     })
     
+    test('Credenciales incorrectas, debe retornar error de autenticación.', async() => {
 
+        store.dispatch.mockReturnValueOnce({ ok: false, message: 'Error en credenciales.' })
+        
+        const wrapper = shallowMount( Login, {
+            global: {
+                plugins: [ store ]
+            }
+        })
+
+        await wrapper.find('form').trigger('submit')
+
+        expect( store.dispatch ).toHaveBeenCalledWith( 'auth/signInUser', {email: '', password: ''} )
+        expect( Swal.fire ).toHaveBeenCalledWith('Error', 'Error en credenciales.', 'error')
+
+    })
+    
 })
